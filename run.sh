3544f3bfc3a80ace1b30a774df654337537c2153
Ryan Miller <ryan@devopsmachine.com>
2020-06-03 20:53:08 -0400

    Add script to run all

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Run all scripts to download and process Pokemon position
+# indicator graphics.
+#
+
+./scripts/download_sprites.sh
+./scripts/generate_element_graphics.sh
+./scripts/generate_pokemon_graphics.sh

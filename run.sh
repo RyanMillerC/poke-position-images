1a1d6739730cde50e041c585e117b3c9b73f8e34
Ryan Miller <ryan@devopsmachine.com>
2020-07-15 22:36:13 -0400

    Print before each sub-script in run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,11 @@
 # indicator graphics.
 #
 
+echo "Downloading sprites..."
 ./scripts/download_sprites.sh
+
+echo "Generating pokemon type graphics..."
 ./scripts/generate_element_graphics.sh
+
+echo "Generating pokemon graphics..."
 ./scripts/generate_pokemon_graphics.sh

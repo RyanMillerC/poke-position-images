2575801d418885a15d8b0ec90ee6c9afb6c3868e
Ryan Miller <ryan@devopsmachine.com>
2020-07-19 10:22:47 -0400

    Use spriteName when downloading if pokemon has it defined

--- scripts/download_sprites.sh
+++ scripts/download_sprites.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 #
-# Download Pokemon sprites.
-#
-# Download Pokemon sprites by first querying PokeAPI for Pokemon
-# names and types, then downloading sprites by name from PokemonDB.net
-# into a local directory.
+# Download Pokemon sprites from PokemonDB.net into a local directory.
 #
 
 BASE_IMAGE_URL='https://img.pokemondb.net/sprites/sword-shield/icon'
@@ -17,12 +13,16 @@ for ((iterator=0; iterator<number_of_pokemon; iterator++)) ; do
     pokemon_name=$(jq -r ".[${iterator}].name" pokemon-data.json)
     pokemon_type=$(jq -r ".[${iterator}].type" pokemon-data.json)
 
+    # Some Pokemon have different sprite file names
+    pokemon_sprite_name=$(jq -r ".[${iterator}].spriteName" pokemon-data.json)
+
     output_directory="${SPRITES_DIRECTORY}/${pokemon_type}"
     [[ ! -d "${output_directory}" ]] && mkdir "${output_directory}"
     output_file="${SPRITES_DIRECTORY}/${pokemon_type}/${pokemon_name}.png"
 
-    url_safe_pokemon_name=$(echo "${pokemon_name}" | tr ' ' '-')
-    pokemon_image_url="${BASE_IMAGE_URL}/${url_safe_pokemon_name}.png"
+    [[ ${pokemon_sprite_name} == 'null' ]] && pokemon_sprite_name="${pokemon_name}"
+    url_safe_pokemon_sprite_name=$(echo "${pokemon_sprite_name}" | tr ' ' '-')
+    pokemon_image_url="${BASE_IMAGE_URL}/${url_safe_pokemon_sprite_name}.png"
     echo "Downloading ${pokemon_image_url} -> ${output_file}"
     curl -s "${pokemon_image_url}" -o "${output_file}"
 
